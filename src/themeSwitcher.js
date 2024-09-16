c09aff9bc05209be3aec696d471fddb6c2bf61dd
Shlaampe <piphencova@icloud.com>
2024-09-16 15:36:09 +0300

    Added JS file

---
+++ src/themeSwitcher.js
@@ -0,0 +1,19 @@
+function themeChange() {
+	const htmlElement = document.documentElement;
+	htmlElement.classList.toggle('dark');
+
+	const theme = htmlElement.classList.contains('dark') ? 'dark' : null;
+	localStorage.setItem('theme', theme);
+}
+
+window.addEventListener(
+	'DOMContentLoaded',
+	() => {
+		const savedTheme = localStorage.getItem('theme');
+		if (savedTheme) {
+			document.documentElement.classList.add(savedTheme);
+		}
+		const switcherButton = document.getElementById('switcherButton');
+		switcherButton.addEventListener('click', themeChange);
+	}
+);
